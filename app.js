118c49d122868ee4ebed4d964abf87f65bd859a1
Andrew Alsing <andrew32alsing@gmail.com>
2023-03-29 14:43:42 -0700

    completed tutorial

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
-
 // Require Libraries
 const express = require('express');
 
 // Require tenorjs near the top of the file
 const Tenor = require("tenorjs").client({
-    // Replace with your own key
-    "Key": "TENOR_API_KEY", // https://tenor.com/developer/keyregistration
-    "Filter": "high", // "off", "low", "medium", "high", not case sensitive
-    "Locale": "en_US", // Your locale here, case-sensitivity depends on input
+  // Replace with your own key
+  "Key": "AIzaSyCfPmjF3cfN-DPvoCGSXpILk36ut0Lp15c",
+  "Filter": "high", // "off", "low", "medium", "high", not case sensitive
+  "Locale": "en_US", // Your locale here, case-sensitivity depends on input
 });
 
 // App Setup
@@ -15,9 +14,8 @@ const app = express();
 app.use(express.static('public'));
 
 // Middleware
-const exphbs  = require('express-handlebars');
-
-app.engine('handlebars', exphbs.engine())
+const exphbs = require('express-handlebars');
+app.engine('handlebars', exphbs.engine());
 app.set('view engine', 'handlebars');
 
 // Routes
@@ -32,13 +30,14 @@ app.get('/', (req, res) => {
         .then(response => {
             // store the gifs we get back from the search
             const gifs = response;
+            console.log(gifs); // Debug line
             // pass the gifs as an object into the home page
             res.render('home', { gifs })
         }).catch(console.error);
-  })
+})
+
 
 // Start Server
-
 app.listen(3000, () => {
   console.log('Gif Search listening on port localhost:3000!');
 });
