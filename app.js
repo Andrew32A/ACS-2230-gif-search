53a9282df88e0ced0d805417fb8b693967201b1f
Andrew Alsing <andrew32alsing@gmail.com>
2023-03-29 14:07:18 -0700

    added search form

--- app.js
+++ app.js
@@ -12,10 +12,8 @@ app.set('view engine', 'handlebars');
 
 // Routes
 app.get('/', (req, res) => {
-    // set the url of the gif
-    const gifUrl = 'https://media1.tenor.com/images/561c988433b8d71d378c9ccb4b719b6c/tenor.gif?itemid=10058245'
-    // render the hello-gif view, passing the gifUrl into the view to be displayed
-    res.render('hello-gif', { gifUrl })
+console.log(req.query) // => "{ term: hey" }[/bold]
+    res.render('home')
 })
 
 // Start Server
@@ -23,3 +21,4 @@ app.get('/', (req, res) => {
 app.listen(3000, () => {
   console.log('Gif Search listening on port localhost:3000!');
 });
+
